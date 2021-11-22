3c3eee017520358b3e8bd6057bfdb6eb5e9f7654
djerror327 <djerror327@gmail.com>
2021-11-22 07:37:10 +0530

    added dashboard initialization

--- dashboardapp/apps.py
+++ dashboardapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PagesConfig(AppConfig):
+class DashboardappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pages'
+    name = 'dashboardapp'
